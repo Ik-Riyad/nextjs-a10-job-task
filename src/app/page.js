c56e518862aa5a769cacb60e00f4f0a0e2b17e1b
Ibrahim Khan Riyadh <ik.riyad123@gmail.com>
2025-08-22 19:18:57 +0600

    work on git

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
       <p className="mt-4 text-lg">
         This is a simple example of a Next.js application using Geist fonts.
